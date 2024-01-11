f3c67e4d93111a4d3b668cf113da54689450fe31
Sean Alvarez <alvie0818@gmail.com>
2024-01-11 16:46:58 -0500

    add basic Az Modules

---
+++ az_sql/variables.tf
@@ -0,0 +1,42 @@
+variable "name" {
+  type = string
+  description = "Name of the SQL DB"
+}
+
+variable "resource_group" {
+  type = string
+  description = "Name of the resource group"
+}
+
+variable "region" {
+  type = string
+  description = "Location of the SQL DB"
+}
+
+variable "max_size_gb" {
+  type = number
+  description = "Maximum size of the SQL DB in GB"
+  default = 32
+}
+
+variable "sku" {
+  type = string
+  description = "SKU of the SQL DB"
+  default = "S0"
+}
+
+variable "zone_redundant" {
+  type = bool
+  description = "Enable zone redundancy"
+  default = false
+}
+
+variable "team_name" {
+  type = string
+  description = "Name of the team"
+}
+
+variable "environment" {
+  type = string
+  description = "Deployment Environment"
+}
---
+++ container_app/main.tf
@@ -0,0 +1,47 @@
+module "tags" {
+    source = "../standard_tags"
+    application = var.app_name
+    team_name = var.team_name
+    environment = var.environment
+}
+
+# This could also be a mandatory parameter
+resource "azurerm_log_analytics_workspace" "app" {
+  name                = var.app_name
+  location            = var.region
+  resource_group_name = var.resource_group
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = module.tags.tags
+}
+
+resource "azurerm_container_app_environment" "app" {
+  name                       = var.app_name
+  location                   = var.region
+  resource_group_name        = var.resource_group
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.app.id
+
+  tags = module.tags.tags
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = var.app_name
+  container_app_environment_id = azurerm_container_app_environment.app.id
+  resource_group_name          = var.resource_group
+  revision_mode                = "Single"
+  tags = module.tags.tags
+
+  template {
+    container {
+      name   = var.container_name
+      image  = var.image_tag
+      cpu    = var.cpu
+      memory = var.memory
+    }
+    max_replicas = var.max_replicas
+    min_replicas = var.min_replicas
+    # NOTE: This doesn't have an init_container block or
+    # other configurations which could be a problem for some users
+  }
+}
